2774f162ab1fb116938536edb47f4323992f3914
Gul Akberova <kbrovagul0@gmail.com>
2023-02-04 14:38:25 +0400

    Group models

---
+++ models/Group.js
@@ -0,0 +1,24 @@
+const { Schema, default: mongoose } = require("mongoose");
+
+const groupSchema=Schema({
+  
+    groupName:{
+        type:String,
+        required:true
+    },
+    isDeleted:{
+        type:Boolean,
+        default:false
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+
+const group=mongoose.model('Group',groupSchema)
+
+module.exports={
+    group
+}
